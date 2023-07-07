ae7eb877358b68ef909f5abc73d69edf0cadad68
Goodnessmbakara <amicablembakara50@gmail.com>
2023-07-07 20:22:05 +0100

    fixed some redundancy in the nurses doctor urls

--- treblle_rest_api_hackathon/patient_records/admin.py
+++ treblle_rest_api_hackathon/patient_records/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from patient_records.models import *
+
+admin.site.site_header = 'Patient Management Webapp'
+
+
+admin.site.register(Drugs)
+
+admin.site.register(Patient)
+
+admin.site.register(Doctors)
+
+admin.site.register(Nurses)
+
+admin.site.register(Billing)
---
+++ treblle_rest_api_hackathon/patient_records/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+
+
+from patient_records.models import *
+
+
+class DrugsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drugs
+        fields  = ['name', 'manufactured_date','expiration_date']
+        
+
+class DoctorsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Doctors
+        fields = '__all__'
+        
+class PatientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Patient
+        fields = '__all__'
+        
+class NurseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Nurses
+        fields = '__all__'
+
+class BillingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Billing
+        fields = '__all__'
